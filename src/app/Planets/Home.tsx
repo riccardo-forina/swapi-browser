5296a7c0eea99dc6fb45959ba118e8b7cdda6886
root <root@localhost.localdomain>
2019-09-18 13:27:06 +0530

    Pass default parameters to usePlanetsService

--- src/app/Planets/Home.tsx
+++ src/app/Planets/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PageSection, Pagination, PaginationVariant } from '@patternfly/react-core';
 import { History } from 'history';
 import { PlanetListing } from '../Planets/PlanetListing';
@@ -13,7 +13,7 @@ const Home: React.FC<IPath> = ({ history }) => {
   const [perPage, setPerPage] = useState(10);
 
   const pageParam = parseInt(Object.keys(queryString.parse(history.location.search))[0]);
-  const { result, page } = usePlanetsService(pageParam);
+  const { result, page } = usePlanetsService(pageParam, 0);
 
   const [itemCount, setItemCount] = useState(0);
   useEffect(() => {
@@ -28,6 +28,7 @@ const Home: React.FC<IPath> = ({ history }) => {
         itemCount={itemCount}
         page={page}
         perPage={perPage}
+        perPageOptions={[{ title: '10', value: 10 }]}
         onSetPage={(_evt, value) => {
           history.push({ pathname: '/Home', search: '?' + value });
         }}
@@ -36,9 +37,7 @@ const Home: React.FC<IPath> = ({ history }) => {
         }}
         variant={PaginationVariant.top}
       />
-      {result.status === 'loaded' && 
-        <PlanetListing planets={result.payload.results} />
-      }
+      {result.status === 'loaded' && <PlanetListing planets={result.payload.results} history={history} />}
       {result.status !== 'loaded' && result.status}
     </PageSection>
   );
