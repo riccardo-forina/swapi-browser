f49bd73bde7599bc23697037d2bddf6b0cf8320b
root <root@localhost.localdomain>
2019-09-18 13:33:41 +0530

    Add new properties for planet details

--- src/app/Types/Planet.tsx
+++ src/app/Types/Planet.tsx
@@ -2,7 +2,17 @@ export interface IPlanet {
   name: string;
   population: string;
   films: string[];
-  index: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  created: string;
+  edited: string;
+  residents: string[];
+  url: string;
 }
 
 export interface IFilm {
